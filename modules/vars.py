bbcdcae431e9d97ab16e658acc15e7ec5af4a2a7
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-01-28 15:00:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "22609670"
 api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-bot_token = "6873339185:AAEyk9G4rqT2a-HE1z3H3KLhCTbKQ0aAb0k"
+bot_token = "6691873935:AAFVmR4imCzJZAC6Gxv_A98m0H0YFlvmn7g"
