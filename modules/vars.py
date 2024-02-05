05aa87f89100752b7b59507494268ff83dd3ba7a
rahul008xy <158485654+rahul008xy@users.noreply.github.com>
2024-02-05 20:48:39 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "22609670"
 api_hash = "3506d8474ad1f4f5e79b7c52a5c3e88d"
-bot_token = "6674386593:AAEl9iguS7ei49AYMBbyXjkQ6GPfN0QB5hw"
+bot_token = "6626670964:AAGk0jf4xeNTJ6taGYZN93G19HwEfBoTiYw"
